11d7f6a90087b5a311d2558bcaee3fffedb2d6cd
vinogray <kategray.work@gmail.com>
2020-08-15 20:55:31 +0300

    2 back commit

--- djreact/urls.py
+++ djreact/urls.py
@@ -1,9 +1,10 @@
 
-from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.conf.urls import include, url
+
 urlpatterns = [
-    url(r'api-auth/', include('rest_framework.urls')),
-    url(r'admin/', admin.site.urls),
+    url('api-auth/', include('rest_framework.urls')),
+    url('admin/', admin.site.urls),
+    url('api/', include('articles.api.urls'))
 ]
- 
